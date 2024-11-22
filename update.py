03453b63a0c322bc8db7d52b55cae854df5e3c3e
EnAnsari <rahmat2022a@gmail.com>
2024-11-22 19:36:59 +0330

    Updated by update.py

--- update.py
+++ update.py
@@ -4,22 +4,22 @@ import os
 def run_command(command, cwd=os.path.dirname(os.path.abspath(__file__))):
     try:
         result = subprocess.run(command, cwd=cwd, check=True, text=True, capture_output=True)
-        # print(result.stdout)
+        print(result.stdout)
     except subprocess.CalledProcessError as e:
-        # print(f"Error while executing {command}:\n{e.stderr}")
+        print(f"Error while executing {command}:\n{e.stderr}")
         pass
 
 def pull_changes():
-    # print("Pulling latest changes...")
+    print("Pulling latest changes...")
     run_command(["git", "pull"])
 
 def commit_changes():
-    # print("Committing changes...")
+    print("Committing changes...")
     run_command(["git", "add", "."])
     run_command(["git", "commit", "-m", 'Updated by update.py'])
 
 def push_changes():
-    # print("Pushing changes...")
+    print("Pushing changes...")
     run_command(["git", "push"])
 
 if __name__ == "__main__":
